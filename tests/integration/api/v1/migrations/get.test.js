d5d3720bcb1809da02dd691cde1014bd73dde866
Danilo Marra Rabelo <danilo2311@gmail.com>
2024-06-11 13:35:31 +0000

    makes npm test more robust with async retry orchestrator.js

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,24 +1,17 @@
 import database from "infra/database.js";
+import orchestrator from "tests/orchestrator.js";
 
-beforeAll(cleanDatabase);
-
-async function cleanDatabase() {
-  await database.query("drop schema public cascade; create schema public");
-}
+beforeAll(async () => {
+  await database.query("drop schema public cascade; create schema public;");
+  await orchestrator.waitForAllServices();
+});
 
 test("GET to /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
   expect(response.status).toBe(200);
 
-  // const isTestEnvironment = process.env.NODE_ENV === "test";
-  // expect(isTestEnvironment).toBe(true);
-  // console.log(process.env.NODE_ENV);
-
-  // const isHostEnvDevelopmentLoaded = process.env.POSTGRES_HOST === "localhost";
-  // expect(isHostEnvDevelopmentLoaded).toBe(true);
-  // console.log(process.env.POSTGRES_HOST);
-
   const responseBody = await response.json();
+
   expect(Array.isArray(responseBody)).toBe(true);
   expect(responseBody.length).toBeGreaterThan(0);
 });
